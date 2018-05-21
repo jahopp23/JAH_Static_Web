da5d3382e45a106629c7c036dc68aaa64d662376
John A. Hopper <jahopp@gmail.com>
2018-05-20 23:14:47 -0500

    late commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   cosmicjs: {
     bucket: {
-      slug: process.env.COSMIC_BUCKET || '5afa3e56c4f4fe66c8fa15e6',
+      slug: process.env.COSMIC_BUCKET || 'static-web-site-personal',
       read_key: process.env.COSMIC_READ_KEY,
       write_key: process.env.COSMIC_WRITE_KEY
     }
